ab5a423107c9a4c112623a8d484a1b718598198b
Ruthellon <58911878+Ruthellon@users.noreply.github.com>
2023-01-17 11:28:01 -0500

    First commit.
    Base board and cards implemented

---
+++ Models/Card.cs
@@ -0,0 +1,39 @@
+﻿using Brainstorm.Models.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Brainstorm.Models
+{
+    public class Card
+    {
+        public CardSuit Suit { get; set; }
+        public CardValue Value { get; set; }
+        public bool IsCreature
+        {
+            get
+            {
+                return Suit == CardSuit.Clubs || Value >= CardValue.Jack || Value == CardValue.Ace;
+            }
+        }
+        public string ImageName { get; set; }
+        public bool IsVisible { get; set; }
+
+        public bool IsRed
+        {
+            get
+            {
+                return Suit == CardSuit.Diamonds || Suit == CardSuit.Hearts;
+            }
+        }
+
+        public bool IsBlack
+        {
+            get
+            {
+                return !IsRed;
+            }
+        }
+    }
+}
---
+++ Models/DiscardPile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Brainstorm.Models
+{
+    public class DiscardPile : PileBase
+    {
+        public List<Card> GetAll()
+        {
+            return Cards.ToList();
+        }
+    }
+}
